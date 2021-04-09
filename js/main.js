985ef67f0b771a57d7b4032e655624267662d755
Rita <rita.rouhban@gmail.com>
2021-04-09 16:51:57 +0200

    fix navbar

--- js/main.js
+++ js/main.js
@@ -69,30 +69,6 @@ AOS.init({
 	};
 	carousel();
 
-	$('nav .dropdown').hover(function(){
-		let $this = $(this);
-		// 	 timer;
-		// clearTimeout(timer);
-		$this.addClass('show');
-		$this.find('> a').attr('aria-expanded', true);
-		// $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
-		$this.find('.dropdown-menu').addClass('show');
-	}, function(){
-		let $this = $(this);
-		// timer;
-		// timer = setTimeout(function(){
-		$this.removeClass('show');
-		$this.find('> a').attr('aria-expanded', false);
-		// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
-		$this.find('.dropdown-menu').removeClass('show');
-		// }, 100);
-	});
-
-
-	$('#dropdown04').on('show.bs.dropdown', function () {
-		console.log('show');
-	});
-
 	// scroll
 	let scrollWindow = function() {
 		$(window).scroll(function(){
